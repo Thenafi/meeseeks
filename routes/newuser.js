90ee1c7b142c59b67c718dabf9e08ced81d20b11
Thenafi <65831153+Thenafi@users.noreply.github.com>
2023-03-15 15:37:42 +0600

    40%

--- routes/newuser.js
+++ routes/newuser.js
@@ -1,12 +1,67 @@
+const Joi = require("joi");
+
 async function routes(fastify, options) {
+  fastify.register(require("@fastify/formbody"));
+
   fastify.get("/", async (request, reply) => {
-    return "On User Page";
+    return reply.view("/templates/newuser.ejs");
   });
 
-  fastify.get("/foo", async function (req, reply) {
-    await this.level.db.put("foo", "bar");
-    const val = await this.level.db.get("foo");
-    return val;
+  fastify.post("/", async function (request, reply) {
+    const { username, password } = request.body;
+
+    // Define the validation schema
+    const schema = Joi.object({
+      username: Joi.string().alphanum().min(4).max(16).required(),
+      password: Joi.string()
+        .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/)
+        .required(),
+    });
+
+    // Validate the request body
+    const { error } = schema.validate(request.body);
+
+    if (error) {
+      // If validation fails, display an error message
+      return reply.view("/templates/message.ejs", {
+        message: "Invalid form data",
+        url: "./",
+        linkText: "Go back",
+      });
+    }
+
+    try {
+      const user = await this.level.db.get(username);
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} already exists`,
+        url: "./newuser",
+        linkText: "Go back",
+      });
+    } catch (err) {
+      const defaultUser = {
+        username: username,
+        password: password,
+        numberOfLinks: 2,
+        links: [
+          "https://source.unsplash.com/random/",
+          "https://source.unsplash.com/random/",
+        ],
+        lastIndex: 0,
+        ttl: 0,
+        random: true,
+      };
+
+      //put the user in the database with the default values as json
+      await this.level.db.put(username, defaultUser, {
+        valueEncoding: "json",
+      });
+
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} created`,
+        url: "./olduser",
+        linkText: "Start Adding",
+      });
+    }
   });
 }
 
--- routes/olduser.js
+++ routes/olduser.js
@@ -1,11 +1,79 @@
 async function routes(fastify, options) {
   fastify.get("/", async (request, reply) => {
-    return "On User Page";
+    return reply.view("/templates/olduser.ejs");
   });
 
+  //settings page
+  fastify.get("/:username/settings", async function (req, reply) {
+    const { username } = req.params;
+    try {
+      const user = await this.level.db.get(username, { valueEncoding: "json" });
+      return reply.view("/templates/settings.ejs", {
+        username: username,
+      });
+    } catch (err) {
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} does not exist`,
+        url: "./",
+        linkText: "Go back",
+      });
+    }
+  });
+
+  // foo for testing
   fastify.get("/foo", async function (req, reply) {
     const val = await this.level.db.get("foo");
-    return val;
+    const val4 = await this.level.db.put("foo3", "sd");
+    try {
+      const val3 = await this.level.db.get("foo3");
+      return val3;
+    } catch (err) {
+      console.log(err);
+      return 1;
+    }
+  });
+
+  //get all user data
+  fastify.get("/:username/:password", async function (req, reply) {
+    const { username, password } = req.params;
+    try {
+      const user = await this.level.db.get(username, { valueEncoding: "json" });
+
+      if (user.password !== password) {
+        return reply.view("/templates/message.ejs", {
+          message: `Password for ${username} is incorrect`,
+          url: "./olduser",
+          linkText: "Go back",
+        });
+      }
+      // console.log(user);
+      return reply.send(user);
+    } catch (err) {
+      console.log(err);
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} does not exist`,
+        url: "./olduser",
+        linkText: "Go back",
+      });
+    }
+  });
+
+  fastify.get("/:username", async function (req, reply) {
+    const { username } = req.params;
+    try {
+      const user = await this.level.db.get(username, { valueEncoding: "json" });
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} exists. To get all details  as json, add /password to the end of the url`,
+        url: `./`,
+        linkText: "Go back",
+      });
+    } catch (err) {
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} does not exist`,
+        url: "./",
+        linkText: "Go back",
+      });
+    }
   });
 }
 
