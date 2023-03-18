355fa2a1a127da455029b039b913c3fd2e0f1473
Thenafi <65831153+Thenafi@users.noreply.github.com>
2023-03-18 13:48:55 +0600

    bug fix

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,11 @@
 // function that converts string true/false to boolean
 
 const convertToBoolean = (string) => {
+  //check if the string is a boolean type
+  if (typeof string === "boolean") {
+    return string;
+  }
+
   if (string.toLowerCase() === "true") {
     return true;
   } else {
