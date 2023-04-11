dae7bd9130e3711a5d85b10a8ec5b506e981b717
Thenafi <65831153+Thenafi@users.noreply.github.com>
2023-04-11 09:33:12 +0600

    Improvements via Chatgpt

--- routes/wish.js
+++ routes/wish.js
@@ -6,77 +6,62 @@ async function routes(fastify, options) {
 
   fastify.get("/", async (request, reply) => {
     return reply.view("/templates/message.ejs", {
-      message: "Please add your username to the url",
+      message: "Please add your username to the URL",
       url: "./",
       linkText: "Go back",
     });
   });
 
   fastify.get("/:username", async (request, reply) => {
-    const username = request.params.username;
+    const username = request.params.username.toLowerCase();
     const userCacheLink = urlCache.get(username);
 
-    if (userCacheLink !== undefined) {
+    if (userCacheLink) {
       reply.redirect(307, userCacheLink);
       return;
-    } else {
-      let link;
-      let lastIndex;
-      const user = await userCollection.findOne(
-        { username: username.toLowerCase() },
-        {
-          projection: { password: 0, _id: 0 },
-        }
-      );
-
-      //to check for existing user
-      if (!user) {
-        return reply.view("/templates/message.ejs", {
-          message: `User ${username} does not exist`,
-          url: "./",
-          linkText: "Go back",
-        });
-      }
-
-      // checking in database and setting cache
-      if (
-        parseInt(user.ttl) > 0 &&
-        !isTimeExpired(user.ttl, user.lastIndexUpdate)
-      ) {
-        reply.redirect(307, user.links[user.lastIndex]);
-        urlCache.set(username, user.links[user.lastIndex], parseInt(user.ttl));
-
-        return;
-      }
-
-      // things that happen after expiry of ttl
-      if (user.random === true) {
-        const randomIndex = getRandomNumberExcluding(
-          user.lastIndex,
-          user.links.length
-        );
-
-        link = user.links[randomIndex];
-        reply.redirect(307, link);
-        lastIndex = randomIndex;
-      } else {
-        lastIndex = user.lastIndex + 1;
-        if (lastIndex >= user.links.length) {
-          lastIndex = 0;
-        }
-        link = user.links[lastIndex];
-        reply.redirect(307, link);
-      }
-
-      await userCollection.updateOne(
-        { username: username.toLowerCase() },
-        { $set: { lastIndex: lastIndex, lastIndexUpdate: new Date() } }
-      );
-      if (parseInt(user.ttl) > 0) {
-        urlCache.set(username, link, parseInt(user.ttl));
-      }
     }
-    return;
+
+    const user = await userCollection.findOne(
+      { username },
+      { projection: { password: 0, _id: 0 } }
+    );
+
+    if (!user) {
+      return reply.view("/templates/message.ejs", {
+        message: `User ${username} does not exist`,
+        url: "./",
+        linkText: "Go back",
+      });
+    }
+
+    const ttl = parseInt(user.ttl);
+    if (ttl > 0 && !isTimeExpired(ttl, user.lastIndexUpdate)) {
+      reply.redirect(307, user.links[user.lastIndex]);
+      urlCache.set(username, user.links[user.lastIndex], ttl);
+      return;
+    }
+
+    let link;
+    let lastIndex;
+
+    if (user.random) {
+      lastIndex = getRandomNumberExcluding(user.lastIndex, user.links.length);
+      link = user.links[lastIndex];
+    } else {
+      lastIndex = (user.lastIndex + 1) % user.links.length;
+      link = user.links[lastIndex];
+    }
+
+    reply.redirect(307, link);
+
+    await userCollection.updateOne(
+      { username },
+      { $set: { lastIndex, lastIndexUpdate: new Date() } }
+    );
+
+    if (ttl > 0) {
+      urlCache.set(username, link, ttl);
+    }
   });
 }
 
